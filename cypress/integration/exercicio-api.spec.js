fc5f7c3fd962772088497b83110705813e4ac245
halttere <matheussena@outlook.com>
2023-06-26 17:04:36 -0300

    Commit exercicios aula 14

--- cypress/integration/exercicio-api.spec.js
+++ cypress/integration/exercicio-api.spec.js
@@ -1,30 +1,64 @@
 /// <reference types="cypress" />
 
-describe('Testes da Funcionalidade Usuários', () => {
+describe("Testes da Funcionalidade Usuários", () => {
+  it("Deve validar contrato de usuários", () => {
+    cy.request("GET", "/usuarios").its("status").should("equal", 200);
+  });
 
-    it('Deve validar contrato de usuários', () => {
-         //TODO: 
+  it("Deve listar usuários cadastrados", () => {
+    cy.request("GET", "/usuarios").its("status").should("equal", 200);
+  });
+
+  it("Deve cadastrar um usuário com sucesso", () => {
+    const novoEmail = internet.email();
+
+    cy.request({
+      method: "POST",
+      url: "/usuarios",
+      body: {
+        nome: "Matheus Sena Aguiar",
+        email: novoEmail,
+        password: "teste",
+        administrador: "true",
+      },
+    }).then((response) => {
+      expect(response.status).to.equal(201);
     });
+  });
 
-    it('Deve listar usuários cadastrados', () => {
-         //TODO: 
+  it("Deve validar um usuário com email inválido", () => {
+    const invalidEmail = "invalidemail";
+
+    cy.request({
+      method: "POST",
+      url: "/usuarios",
+      failOnStatusCode: false,
+      body: {
+        name: "Matheus Neves dos Santos",
+        email: invalidEmail,
+        password: "senha123",
+      },
+    }).then((response) => {
+      expect(response.status).to.equal(400);
     });
+  });
 
-    it('Deve cadastrar um usuário com sucesso', () => {
-         //TODO: 
-    });
+  it("Deve editar um usuário previamente cadastrado", () => {
+    const updatedUserData = {
+      nome: "Matheus Sena Aguiar",
+      email: "1111122000@outlook.com",
+      password: "teste",
+      administrador: "true",
+    };
 
-    it('Deve validar um usuário com email inválido', () => {
-         //TODO: 
-    });
+    cy.request("PUT", `/usuarios/put_usuarios___id_`, updatedUserData)
+      .its("status")
+      .should("equal", 201);
+  });
 
-    it('Deve editar um usuário previamente cadastrado', () => {
-         //TODO: 
-    });
-
-    it('Deve deletar um usuário previamente cadastrado', () => {
-        //TODO: 
-    });
-
-
+  it("Deve deletar um usuário previamente cadastrado", () => {
+    cy.request("DELETE", `/usuarios/delete_usuarios__id_`)
+      .its("status")
+      .should("equal", 200);
+  });
 });
