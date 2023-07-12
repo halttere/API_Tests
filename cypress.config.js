4a528b488bce48686b69f5a2367b37c8a4060fd3
halttere <matheussena@outlook.com>
2023-07-12 11:44:31 -0300

    Atualizaçao de funcionalidades

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  e2e: {
+    baseUrl: "http://localhost:3000/",
+  },
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "mochawesome-report",
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+};
